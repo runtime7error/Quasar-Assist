2d851ad3a2a83aeb94b8556c34672f74d9915fd9
lucas_henri <validacao01.inobag@gmail.com>
2021-06-23 09:12:26 -0300

    responsive_buttons

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,12 +3,12 @@
     <q-header elevation="0">
       <q-toolbar class="header">
         <q-toolbar-title class="text-h6"><strong>RPG</strong>Assist</q-toolbar-title>
-        <div class="desktop-only">
+        <div class="mobile-hide">
           <q-btn rounded flat label="Home" class="q-ml-xs"/>
           <q-btn rounded flat label="Criar" class="q-ml-xs" @click="create()"/>
           <q-btn rounded flat label="Fichas" class="q-ml-xs"/>
         </div>  
-        <div class="desktop-only">
+        <div class="mobile-hide">
           <q-input dense rounded outlined v-model="text" color="purple-4" bg-color="white" placeholder="Digite sua busca">
             <template v-slot:append>
               <q-icon v-if="text === ''" name="search" /> 
