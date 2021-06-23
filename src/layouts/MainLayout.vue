e9b8ff18862b3bbfeb1847e1e31e851629eee917
lucas_henri <validacao01.inobag@gmail.com>
2021-06-23 09:36:41 -0300

    responsive_toolbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,13 +2,16 @@
   <q-layout view="lHh Lpr LFf" class="header">
     <q-header elevation="0">
       <q-toolbar class="header">
-        <q-toolbar-title class="text-h6"><strong>RPG</strong>Assist</q-toolbar-title>
-        <div class="mobile-hide">
+        <div>
+          <q-toolbar-title class="text-h6"><strong>RPG</strong>Assist</q-toolbar-title>
+        </div>
+        <q-space></q-space>
+        <div class="gt-xs">
           <q-btn rounded flat label="Home" class="q-ml-xs"/>
           <q-btn rounded flat label="Criar" class="q-ml-xs" @click="create()"/>
           <q-btn rounded flat label="Fichas" class="q-ml-xs"/>
         </div>  
-        <div class="mobile-hide">
+        <div class="gt-xs">
           <q-input dense rounded outlined v-model="text" color="purple-4" bg-color="white" placeholder="Digite sua busca">
             <template v-slot:append>
               <q-icon v-if="text === ''" name="search" /> 
@@ -16,9 +19,22 @@
             </template>
           </q-input>
         </div>
+        <div class="lt-sm">
+          <q-btn
+            flat
+            dense
+            round
+            icon="menu"
+            aria-label="menu"
+            > 
+          </q-btn>
+        </div>
       </q-toolbar>
     </q-header>
-   
+   <q-drawer
+   >
+
+   </q-drawer>
     <q-page-container v-if="!ficha" class="row text-h5 justify-center">
       <div class="fixed-center">
         <q-card class="null-card">
