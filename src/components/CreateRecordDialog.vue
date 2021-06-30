5631d2c301a44993b7b7a8722e016fb2a485dba3
Nxrth-x <devilsouls666@gmail.com>
2021-06-30 06:03:27 -0300

    Fixes in the localbase and componentization Localbase queries fixes Component lifecycle fixes Componentization '.prettierrc' file added

---
+++ src/components/CreateRecordDialog.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="q-pa-md null-card dialog-border" style="max-width: 400px">
+    <q-form @submit="handleSave" @reset="handleClose" class="q-gutter-md">
+      <q-card-section>
+        <div class="text-h6 q-gutter-md">Criação de Ficha</div>
+      </q-card-section>
+      <!-- Nome -->
+      <q-input
+        dense
+        rounded
+        outlined
+        color="purple-4"
+        bg-color="white"
+        placeholder="Nome"
+        lazy-rules
+        v-model="record.name"
+        :rules="[
+          value => (value && value.length > 0) || 'Nome não pode ficar vazio!',
+        ]"
+        required
+      />
+      <!-- Idade -->
+      <q-input
+        dense
+        rounded
+        outlined
+        color="purple-4"
+        bg-color="white"
+        placeholder="Idade"
+        mask="#########"
+        lazy-rules
+        v-model="record.age"
+        :rules="[
+          value => (value !== '' && +value > 0) || 'Por favor digite a idade',
+        ]"
+        required
+      />
+      <!-- Raça -->
+      <q-input
+        dense
+        rounded
+        outlined
+        color="purple-4"
+        bg-color="white"
+        placeholder="Raça"
+        lazy-rules
+        v-model="record.breed"
+        :rules="[
+          value => (value && value.length > 0) || 'Raça não pode ficar vazio !',
+        ]"
+        required
+      />
+
+      <div>
+        <q-btn color="red" label="Cancelar" rounded type="reset" />
+        <q-btn
+          color="green"
+          label="Salvar"
+          rounded
+          type="submit"
+          class="q-ml-xl"
+        />
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['create', 'close'],
+  data: () => ({
+    display: true,
+    record: {
+      name: '',
+      age: '',
+      breed: '',
+    },
+  }),
+  methods: {
+    handleSave() {
+      this.$emit('create', this.record)
+    },
+    handleClose() {
+      this.$emit('close')
+    },
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/RecordCardsContainer.vue
@@ -0,0 +1,61 @@
+<template>
+  <div style="position: relative; top: 100px">
+    <q-page-container class="row justify-start content-end">
+      <div
+        class="q-ma-lg"
+        style="margin-right: 30px"
+        :key="record.id"
+        v-for="record in records"
+        @click="handleClick(record)"
+      >
+        <q-card class="dialog-border null-card">
+          <q-input
+            :label="record.name"
+            class="q-ma-lg"
+            dense
+            rounded
+            outlined
+            color="purple-4"
+            bg-color="white"
+            readonly
+          />
+          <q-input
+            class="q-ma-lg"
+            dense
+            rounded
+            outlined
+            color="purple-4"
+            bg-color="white"
+            readonly
+            :label="record.age"
+          />
+          <q-input
+            :label="record.breed"
+            class="q-ma-lg"
+            dense
+            rounded
+            outlined
+            color="purple-4"
+            bg-color="white"
+            readonly
+          />
+        </q-card>
+      </div>
+    </q-page-container>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['records'],
+  emits: ['showRecord'],
+  methods: {
+    handleClick(record) {
+      this.$emit('showRecord', record)
+    },
+  },
+}
+</script>
+
+<style>
+</style>
