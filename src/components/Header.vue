db1c6ba3b525652e1877f1cc16eddd43232b7a4e
lucas_henri <validacao01.inobag@gmail.com>
2021-06-30 10:20:08 -0300

    responsive-menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,44 +1,75 @@
 <template>
-  <q-header elevation="0">
-    <q-toolbar class="header">
-      <div>
-        <q-toolbar-title class="text-h6">
-          <strong>RPG</strong>Assist
-        </q-toolbar-title>
-      </div>
-      <q-space></q-space>
-      <div class="gt-xs">
-        <q-btn rounded flat label="Home" class="q-ml-xs" />
-        <q-btn
-          rounded
-          flat
-          label="Criar"
-          class="q-ml-xs"
-          @click="handleClick"
-        />
-        <q-btn rounded flat label="Fichas" class="q-ml-xs" />
-      </div>
-      <div class="gt-xs">
-        <q-input
-          dense
-          rounded
-          outlined
-          v-model="text"
-          color="purple-4"
-          bg-color="white"
-          placeholder="Digite sua busca"
-        >
-          <template v-slot:append>
-            <q-icon v-if="text === ''" name="search" />
-            <q-icon v-else name="clear" @click="text = ''" />
-          </template>
-        </q-input>
-      </div>
-      <div class="lt-sm">
-        <q-btn flat dense round icon="menu" aria-label="menu"> </q-btn>
-      </div>
-    </q-toolbar>
-  </q-header>
+  <div>
+    <q-header elevation="0">
+      <q-toolbar class="header">
+        <div>
+          <q-toolbar-title class="text-h6">
+            <strong>RPG</strong>Assist
+          </q-toolbar-title>
+        </div>
+        <q-space></q-space>
+        <div class="gt-xs">
+          <q-btn rounded flat label="Home" class="q-mr-xs" />
+          <q-btn
+            rounded
+            flat
+            label="Criar"
+            class="q-ml-xs"
+            @click="handleClick"
+          />
+          <q-btn rounded flat label="Fichas" class="q-mr-xs" />
+        </div>
+        <div class="gt-xs">
+          <q-input
+            dense
+            rounded
+            outlined
+            v-model="text"
+            color="purple-4"
+            bg-color="white"
+            placeholder="Digite sua busca"
+          >
+            <template v-slot:append>
+              <q-icon v-if="text === ''" name="search" />
+              <q-icon v-else name="clear" @click="text = ''" />
+            </template>
+          </q-input>
+        </div>
+        <div class="lt-sm">
+          <q-btn @click="drawer = !drawer" flat dense round icon="menu" aria-label="menu"> </q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
+    <q-drawer
+      class="lt-sm"
+      v-model="drawer"
+      side="right"
+      :width="200"
+      :breakpoint="100"
+      overlay
+      elevated
+      content-class="header"
+      behavior="mobile"
+
+    >
+      <q-list>
+        <q-item>
+          <q-item-section>
+            <q-btn label="Home"  />
+          </q-item-section>
+        </q-item>
+        <q-item>
+          <q-item-section>
+            <q-btn
+      
+              label="Criar"
+              @click="handleClick"
+            />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+  </div>
 </template>
 
 <script>
@@ -46,6 +77,7 @@ export default {
   emits: ['createRecord'],
   data: () => ({
     text: '',
+    drawer: false,
   }),
   methods: {
     handleClick() {
